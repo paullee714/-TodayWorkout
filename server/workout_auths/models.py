824e99bf1deb319f9c061f0d37acd4a8d0d61081
paullee714 <paul_lee@kakao.com>
2024-01-03 00:34:38 +0900

    feat: remove unuse import

--- server/workout_auths/models.py
+++ server/workout_auths/models.py
@@ -1,6 +1,3 @@
-import random
-import string
-
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
